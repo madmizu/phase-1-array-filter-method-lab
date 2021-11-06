5f23bb2262ab115df07e84baff903c35ef5431d1
Madison Shimizu <mhshimizu@outlook.com>
2021-11-05 22:49:01 -1000

    commit tree 4a1c3f9e4ef4997b8e40ff2bee80303d0c183d43

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+
+
+const findMatching = (array, name) => array.filter(driver => driver.toLowerCase() === name.toLowerCase());
+
+const fuzzyMatch = (array, letters) => array.filter (members => members.charAt() === letters.charAt())
+
+// QUESTION: how does JS know that 'record' refers to 
+
+const matchName = (array, memberName) => array.filter (nm => nm.name === memberName);
